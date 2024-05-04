89520049e71515e55f884d4854a8523572522b3e
James Aprosail <aprosail@outlook.com>
2024-05-04 23:32:30 +0800

    example follow specification

--- example/standard_example.dart
+++ example/standard_example.dart
@@ -1,14 +1,16 @@
 import 'package:terminal_decorate/terminal_decorate.dart';
 
-void main() {
+void standardExample() {
   for (final item in fontStyleDecorations) print(item);
 }
 
 final fontStyleDecorations = [
   '\nfont style decorations:',
   '(${Escape.bold.code}:${Escape.cancelBoldOrFaint.code}) ${'bold'.bold} style',
-  '(${Escape.faint.code}:${Escape.cancelBoldOrFaint.code}) ${'faint'.faint} style',
-  '(${Escape.italic.code}:${Escape.cancelItalic.code}) ${'italic'.italic} style',
+  '(${Escape.faint.code}:${Escape.cancelBoldOrFaint.code}) '
+      '${'faint/dim'.faint} style',
+  '(${Escape.italic.code}:${Escape.cancelItalic.code}) '
+      '${'italic'.italic} style',
   '(${Escape.underline.code}:${Escape.cancelUnderline.code}) '
       '${'underline'.underline} style',
   '(${Escape.blink.code}:${Escape.cancelBlink.code}) '
@@ -20,7 +22,7 @@ final fontStyleDecorations = [
   '(${Escape.negative.code}:${Escape.cancelNegative.code}) '
       '${'negative'.negative} style',
   '(${Escape.conceal.code}:${Escape.cancelConceal.code}) '
-      '${'conceal'.conceal} style',
+      '${'conceal'.conceal} (conceal/hide) style',
   '(${Escape.crossLine.code}:${Escape.cancelCrossLine.code}) '
       '${'cross line'.crossLine} style',
   '(${Escape.doubleUnderline.code}:${Escape.cancelUnderline.code}) '
