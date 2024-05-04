3e5ab35d73c7ca67f738fe94ae43c1f13bef3945
James Aprosail <aprosail@outlook.com>
2024-05-05 00:02:20 +0800

    foreground and background

--- example/standard_example.dart
+++ example/standard_example.dart
@@ -2,6 +2,8 @@ import 'package:terminal_decorate/terminal_decorate.dart';
 
 void standardExample() {
   for (final item in fontStyleDecorations) print(item);
+  for (final item in foregroundDecorations) print(item);
+  for (final item in backgroundDecorations) print(item);
 }
 
 final fontStyleDecorations = [
@@ -28,4 +30,36 @@ final fontStyleDecorations = [
   '(${Escape.doubleUnderline.code}:${Escape.cancelUnderline.code}) '
       '${'double underline'.doubleUnderline} style '
       '(usually unsupported)',
+];
+
+final foregroundDecorations = [
+  '(${Escape.black.code}:${Escape.cancelFg.code}) '
+      '${'black'.black} (black) style (sometimes deep gray)',
+  '(${Escape.red.code}:${Escape.cancelFg.code}) ${'red'.red} style',
+  '(${Escape.yellow.code}:${Escape.cancelFg.code}) ${'yellow'.yellow} style',
+  '(${Escape.green.code}:${Escape.cancelFg.code}) ${'green'.green} style',
+  '(${Escape.cyan.code}:${Escape.cancelFg.code}) ${'cyan'.cyan} style',
+  '(${Escape.blue.code}:${Escape.cancelFg.code}) ${'blue'.blue} style',
+  '(${Escape.magenta.code}:${Escape.cancelFg.code}) ${'magenta'.magenta} style',
+  '(${Escape.white.code}:${Escape.cancelFg.code}) '
+      '${'white'.white} (white) style',
+];
+
+final backgroundDecorations = [
+  '(${Escape.bgBlack.code}:${Escape.cancelBg.code}) '
+      '${'black background'.bgBlack} (black) style',
+  '(${Escape.bgRed.code}:${Escape.cancelBg.code}) '
+      '${'red background'.bgRed} style',
+  '(${Escape.bgYellow.code}:${Escape.cancelBg.code}) '
+      '${'yellow background'.bgYellow} style',
+  '(${Escape.bgGreen.code}:${Escape.cancelBg.code}) '
+      '${'green background'.bgGreen} style',
+  '(${Escape.bgCyan.code}:${Escape.cancelBg.code}) '
+      '${'cyan background'.bgCyan} style',
+  '(${Escape.bgBlue.code}:${Escape.cancelBg.code}) '
+      '${'blue background'.bgBlue} style',
+  '(${Escape.bgMagenta.code}:${Escape.cancelBg.code}) '
+      '${'magenta background'.bgMagenta} style',
+  '(${Escape.bgWhite.code}:${Escape.cancelBg.code}) '
+      '${'white background'.bgWhite} (white) style',
 ];
--- lib/src/standard.dart
+++ lib/src/standard.dart
@@ -1,7 +1,4 @@
 enum Escape {
-  // || ==================================================================== ||
-  // ||                        Font style decorations                        ||
-  // || ==================================================================== ||
   cancelAll(0),
   bold(1),
 
@@ -26,16 +23,38 @@ enum Escape {
   /// Usually unsupported.
   doubleUnderline(21),
 
-  // || ==================================================================== ||
-  // ||                    Cancel font style decorations                     ||
-  // || ==================================================================== ||
+  // Cancel font decorations.
   cancelBoldOrFaint(22),
   cancelItalic(23),
   cancelUnderline(24),
   cancelBlink(25),
   cancelNegative(27),
   cancelConceal(28),
-  cancelCrossLine(29);
+  cancelCrossLine(29),
+
+  // Foreground decorations.
+  black(30),
+  red(31),
+  green(32),
+  yellow(33),
+  blue(34),
+  magenta(35),
+  cyan(36),
+  white(37),
+  customizeFg(38),
+  cancelFg(39),
+
+  // Bg decoration.
+  bgBlack(40),
+  bgRed(41),
+  bgGreen(42),
+  bgYellow(43),
+  bgBlue(44),
+  bgMagenta(45),
+  bgCyan(46),
+  bgWhite(47),
+  customizeBg(48),
+  cancelBg(49);
 
   const Escape(this.code)
       : assert(code >= 0 && code <= 0xff, 'invalid code: $code (0~255)');
@@ -48,8 +67,7 @@ enum Escape {
   String toString() => format;
 }
 
-extension TerminalDecorationBasic on String {
-  // Font style decorations.
+extension TerminalFontStyleDecorations on String {
   String get bold => '${Escape.bold}$this${Escape.cancelBoldOrFaint}';
   String get faint => '${Escape.faint}$this${Escape.cancelBoldOrFaint}';
   String get italic => '${Escape.italic}$this${Escape.cancelItalic}';
@@ -65,4 +83,26 @@ extension TerminalDecorationBasic on String {
   // Syntax sugars.
   String get dim => faint;
   String get hide => conceal;
+}
+
+extension TerminalColorDecorations on String {
+  // Foreground.
+  String get black => '${Escape.black}$this${Escape.cancelFg}';
+  String get red => '${Escape.red}$this${Escape.cancelFg}';
+  String get green => '${Escape.green}$this${Escape.cancelFg}';
+  String get yellow => '${Escape.yellow}$this${Escape.cancelFg}';
+  String get blue => '${Escape.blue}$this${Escape.cancelFg}';
+  String get magenta => '${Escape.magenta}$this${Escape.cancelFg}';
+  String get cyan => '${Escape.cyan}$this${Escape.cancelFg}';
+  String get white => '${Escape.white}$this${Escape.cancelFg}';
+
+  // Background.
+  String get bgBlack => '${Escape.bgBlack}$this${Escape.cancelBg}';
+  String get bgRed => '${Escape.bgRed}$this${Escape.cancelBg}';
+  String get bgGreen => '${Escape.bgGreen}$this${Escape.cancelBg}';
+  String get bgYellow => '${Escape.bgYellow}$this${Escape.cancelBg}';
+  String get bgBlue => '${Escape.bgBlue}$this${Escape.cancelBg}';
+  String get bgMagenta => '${Escape.bgMagenta}$this${Escape.cancelBg}';
+  String get bgCyan => '${Escape.bgCyan}$this${Escape.cancelBg}';
+  String get bgWhite => '${Escape.bgWhite}$this${Escape.cancelBg}';
 }
