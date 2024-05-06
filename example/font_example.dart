13dcf0a4b0c6a474e640ed77196f3b2d25b96b32
James Aprosail <aprosail@outlook.com>
2024-05-06 08:43:11 +0800

    format font example

--- example/font_example.dart
+++ example/font_example.dart
@@ -5,26 +5,68 @@ void fontExample() {
 }
 
 final basicFontDecorations = [
-  '(${Escape.bold.code}:${Escape.cancelBoldOrFaint.code}) ${'bold'.bold} style',
-  '(${Escape.faint.code}:${Escape.cancelBoldOrFaint.code}) '
-      '${'faint/dim'.faint} style',
-  '(${Escape.italic.code}:${Escape.cancelItalic.code}) '
-      '${'italic'.italic} style',
-  '(${Escape.underline.code}:${Escape.cancelUnderline.code}) '
-      '${'underline'.underline} style',
-  '(${Escape.blink.code}:${Escape.cancelBlink.code}) '
-      '${'blink'.blink} style '
-      '(usually unsupported)',
-  '(${Escape.rapidBlink.code}:${Escape.cancelBlink.code}) '
-      '${'rapid blink'.rapidBlink} style '
-      '(usually unsupported)',
-  '(${Escape.negative.code}:${Escape.cancelNegative.code}) '
-      '${'negative'.negative} style',
-  '(${Escape.conceal.code}:${Escape.cancelConceal.code}) '
-      '${'conceal'.conceal} (conceal/hide) style',
-  '(${Escape.crossLine.code}:${Escape.cancelCrossLine.code}) '
-      '${'cross line'.crossLine} style',
-  '(${Escape.doubleUnderline.code}:${Escape.cancelUnderline.code}) '
-      '${'double underline'.doubleUnderline} style '
-      '(usually unsupported)',
+  singleFontLine(
+    prefix: Escape.bold,
+    suffix: Escape.cancelBoldOrFaint,
+    content: 'bold'.bold,
+  ),
+  singleFontLine(
+    prefix: Escape.faint,
+    suffix: Escape.cancelBoldOrFaint,
+    content: 'faint/dim'.faint,
+  ),
+  singleFontLine(
+    prefix: Escape.italic,
+    suffix: Escape.cancelItalic,
+    content: 'italic'.italic,
+  ),
+  singleFontLine(
+    prefix: Escape.underline,
+    suffix: Escape.cancelUnderline,
+    content: 'underline'.underline,
+  ),
+  singleFontLine(
+    prefix: Escape.blink,
+    suffix: Escape.cancelBlink,
+    content: 'blink'.blink,
+    comment: 'usually unsupported',
+  ),
+  singleFontLine(
+    prefix: Escape.rapidBlink,
+    suffix: Escape.cancelBlink,
+    content: 'rapid blink'.rapidBlink,
+    comment: 'usually unsupported',
+  ),
+  singleFontLine(
+    prefix: Escape.negative,
+    suffix: Escape.cancelNegative,
+    content: 'negative'.negative,
+  ),
+  singleFontLine(
+    prefix: Escape.conceal,
+    suffix: Escape.cancelConceal,
+    content: 'conceal'.conceal,
+    comment: 'conceal/hide',
+  ),
+  singleFontLine(
+    prefix: Escape.crossLine,
+    suffix: Escape.cancelCrossLine,
+    content: 'cross line'.crossLine,
+  ),
+  singleFontLine(
+    prefix: Escape.doubleUnderline,
+    suffix: Escape.cancelUnderline,
+    content: 'double underline'.doubleUnderline,
+    comment: 'usually unsupported',
+  ),
 ];
+
+String singleFontLine({
+  required Escape prefix,
+  required Escape suffix,
+  required String content,
+  String? comment,
+}) {
+  return '(${prefix.code}:${suffix.code}) $content style '
+      '${comment != null ? '($comment)' : ''}';
+}
