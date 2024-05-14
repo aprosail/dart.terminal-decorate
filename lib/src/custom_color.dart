d1c5cbbd0a6d1c3d9a9a7f465def776b18ed151e
James Aprosail <aprosail@outlook.com>
2024-05-14 16:34:36 +0800

    custom color using code and rgb

---
+++ lib/src/custom_color.dart
@@ -0,0 +1,38 @@
+import 'escape.dart';
+
+extension CustomTypeCode on int {
+  static const code = 5;
+  static const rgb = 2;
+}
+
+extension CustomizeColor on String {
+  String foregroundCode(int code) => resolveNestEscape(
+        Escape(ForegroundCode.custom, parameters: [CustomTypeCode.code, code]),
+        Escape(CancelCode.foreground),
+        overrides: ForegroundCode.all,
+      );
+
+  String backgroundCode(int code) => resolveNestEscape(
+        Escape(ForegroundCode.custom, parameters: [CustomTypeCode.code, code]),
+        Escape(CancelCode.background),
+        overrides: BackgroundCode.all,
+      );
+
+  String foregroundRGB(int red, int green, int blue) => resolveNestEscape(
+        Escape(
+          ForegroundCode.custom,
+          parameters: [CustomTypeCode.rgb, red, green, blue],
+        ),
+        Escape(CancelCode.foreground),
+        overrides: ForegroundCode.all,
+      );
+
+  String backgroundRGB(int red, int green, int blue) => resolveNestEscape(
+        Escape(
+          BackgroundCode.custom,
+          parameters: [CustomTypeCode.rgb, red, green, blue],
+        ),
+        Escape(CancelCode.background),
+        overrides: BackgroundCode.all,
+      );
+}
